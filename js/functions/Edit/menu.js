44dc32213fdc8143343f26c0403941ce12282a94
aformoso <adrien.formoso.af@gmail.com>
2023-02-13 16:04:12 +0100

    menu jouer origin

--- js/functions/Edit/menu.js
+++ js/functions/Edit/menu.js
@@ -1,4 +1,19 @@
-function menu (){
-    console.log("coucou")
+const madiv = document.createElement("div");
+madiv.style.width = "100%";
+madiv.style.height = "100vh";
+
+madiv.id = "madiv"
+document.querySelector(".game-content").appendChild(madiv)
+
+
+function menu () {
+    
+    menuJouer()
+
 };
+
+function menuJouer () {
+    console.log("hello_world")
+}
+
 export default menu;
