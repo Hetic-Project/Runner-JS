1ffc9c9fe87231ae2023d506492a2eada1cda811
ken-william <theophanewilliam@gmail.com>
2023-02-14 10:11:58 +0100

    main

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import menu from './functions/Edit/menu.js'
+import subMenuEdition from './functions/Edit/subMenuEdition.js'
 import subMenuPause from './functions/Edit/subMenuPause.js'
 menu()
-subMenuPause
+subMenuPause()
+subMenuEdition()
 
 
