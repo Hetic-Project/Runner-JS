94b8828cb8cae082a5f38c2d81dae750437bdd29
aformoso <adrien.formoso.af@gmail.com>
2023-02-16 12:45:48 +0100

    menu game over v1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-import {menu} from './functions/Edit/menu.js'
-import subMenuEdition from './functions/Edit/subMenuEdition.js'
-import subMenuPause from './functions/Edit/subMenuPause.js'
-import runLevel from './functions/Running/runLevel.js';
-menu()
+// import {menu} from './functions/Edit/menu.js'
+// import subMenuEdition from './functions/Edit/subMenuEdition.js'
+import subMenuGameover from './functions/Edit/subMenuGameover.js';
+// import subMenuPause from './functions/Edit/subMenuPause.js'
+// import runLevel from './functions/Running/runLevel.js';
+subMenuGameover()
 // subMenuEdition()
 // subMenuPause()
 
