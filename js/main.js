485db1fbe8feb95edc12c424b6a98addfc33ea19
Vandal-William <vandal.william@hotmail.com>
2023-02-18 17:44:00 +0100

    bug

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 import {menu} from './functions/Edit/menu.js'
-import subMenuEdition from './functions/Edit/subMenuEdition.js'
-import subMenuFinish from "./functions/Edit/subMenuFinish.js"
-import subMenuPause from './functions/Edit/subMenuPause.js'
 menu()
 
 
